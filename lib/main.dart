37e06bcadb88b6b16c29451e401acfeb7062f5a2
Higinio Santos <hrsp0001@ce.pucmm.edu.do>
2023-11-02 14:49:33 -0400

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
 import 'package:pokedex_proyecto_final/home_screen.dart';
 import 'package:pokedex_proyecto_final/poke_database.dart';
-import 'package:sqflite/sqflite.dart';
-import 'dart:async';
 import 'package:flutter/widgets.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'package:path_provider/path_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Llama a insertPokemonsFromApi para insertar los datos en la base de datos
   await PokeDatabase.instance.insertPokemonsFromApi();
 
-  // Llama a printAllPokemons para imprimir los datos de la base de datos
   await PokeDatabase.instance.printAllPokemons();
 
-  // Resto del código de tu aplicación Flutter
   runApp(MyApp());
 }
 
